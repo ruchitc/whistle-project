b8121c57a2b5dff79d64879bf5ac05c686a6bc49
ruchitc <ruchit.contractor@gmail.com>
2023-04-23 17:44:05 +0530

    Add ml code to registry

--- backend/config/apps/ml/tests.py
+++ backend/config/apps/ml/tests.py
@@ -1,8 +1,10 @@
 from django.test import  TestCase
+import inspect
 
 import pandas as pd
 
 from apps.ml.content_based_rec.recommender import Recommender
+from apps.ml.registry import MLRegistry
 
 class MLTests(TestCase):
 
@@ -17,3 +19,22 @@ class MLTests(TestCase):
         response = recommender.computeRecommendation(input_data)
         
         self.assertEqual('OK', response['status'])
+
+    # test registry
+    def test_registry(self):
+        registry = MLRegistry()
+        self.assertEqual(len(registry.endpoints), 0)
+        endpoint_name = "content_based_rec"
+        algorithm_object = Recommender()
+        algorithm_name = "cosine_similarity"
+        algorithm_status = "production"
+        algorithm_version = "0.0.1"
+        algorithm_owner = "ruchit"
+        algorithm_description = "Music recommender using cosine similarity"
+        algorithm_code = inspect.getsource(Recommender)
+        # add to registry
+        registry.add_algorithm(endpoint_name, algorithm_object, algorithm_name,
+                    algorithm_status, algorithm_version, algorithm_owner,
+                    algorithm_description, algorithm_code)
+        # there should be one endpoint available
+        self.assertEqual(len(registry.endpoints), 1)
--- backend/config/config/wsgi.py
+++ backend/config/config/wsgi.py
@@ -14,3 +14,25 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+# ML registry
+import inspect
+from apps.ml.registry import MLRegistry
+from apps.ml.content_based_rec.recommender import Recommender
+
+try:
+    registry = MLRegistry() # create ML registry
+    # Random Forest classifier
+    rec = Recommender()
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="content_based_rec",
+                            algorithm_object=rec,
+                            algorithm_name="cosine_similarity",
+                            algorithm_status="production",
+                            algorithm_version="0.0.1",
+                            owner="ruchit",
+                            algorithm_description="Music recommender using cosine similarity",
+                            algorithm_code=inspect.getsource(Recommender))
+
+except Exception as e:
+    print("Exception while loading the algorithms to the registry,", str(e))
